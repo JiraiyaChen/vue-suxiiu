c1f7e6e45a35152e20b3112a29a82ceb3f0e7d9b
JiraiyaChen <979526234@qq.com>
2026-04-14 14:06:49 +0800

    5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.65, 0.9], speed: 4 },
+      svgo: { plugins: [{ name: 'removeViewBox' }] },
+    }),
+  ],
   base: '/vue-suxiiu/',
 });
